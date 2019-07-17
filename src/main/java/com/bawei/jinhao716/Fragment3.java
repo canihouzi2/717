c9a0387656bb9288a910ae68f67be941c9cc9be9
canihouzi2 <1269733973@qq.com>
2019-07-17 09:03:26 +0800

    first commit

---
+++ src/main/java/com/bawei/jinhao716/Fragment3.java
@@ -0,0 +1,27 @@
+package com.bawei.jinhao716;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/*
+ *@Auther:Lenovo
+ *@Date: 2019/7/16
+ *@Time: 08:57:48
+ *@Description:
+ * */public class Fragment3 extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View inflate = inflater.inflate(R.layout.fragment3, container, false);
+        return inflate;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
